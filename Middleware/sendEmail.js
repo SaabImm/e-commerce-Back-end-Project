726a93075c2610db42fc1c9daebd8bbcb0596b75
SaaImm <saabimm@gmail.com>
2025-11-05 16:30:17 +0100

    email error again

--- Middleware/sendEmail.js
+++ Middleware/sendEmail.js
@@ -3,11 +3,11 @@ import { Resend } from "resend";
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 export const sendVerificationEmail = async (email, token) => {
-  const verifyUrl = `${process.env.VITE_API_URL}/verify?token=${token}`;
+  const verifyUrl = `https://back-end-signup-and-login.onrender.com/verify?token=${token}`;
 
   try {
     await resend.emails.send({
-      from: process.env.EMAIL_USER,
+      from: "onboarding@resend.dev",
       to: email,
       subject: "Verify your email",
       html: `
@@ -18,5 +18,7 @@ export const sendVerificationEmail = async (email, token) => {
     console.log("✅ Verification email sent to:", email);
   } catch (error) {
     console.error("❌ Error sending email:", error);
+    throw error;
   }
+  
 };
