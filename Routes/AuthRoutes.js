d1cc2299d9c3ec9bf09cb6441f20a7596c87d908
SaaImm <saabimm@gmail.com>
2025-11-05 16:38:36 +0100

    email error again and again

--- Routes/AuthRoutes.js
+++ Routes/AuthRoutes.js
@@ -3,9 +3,8 @@ const router = express.Router();
 
 const signUp= require('../Middleware/SignUp')
 const login= require('../Middleware/Login')
-const verifyEmail= require("../Middleware/verifyEmail")
-
+const sendVerificationEmail = require("../Middleware/sendEmail")
 router.post('/signup', signUp.signup);
-router.get("/verify/:token",verifyEmail.verifyEmail);
+router.get("/verify/:token",sendVerificationEmail.sendVerificationEmail);
 router.post('/login', login.login);
 module.exports = router;
