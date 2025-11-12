7ddb1d5174f7ecb0fa36289b5470799f525cb983
unknown <saabimm@gmail.com>
2025-11-12 11:34:10 +0100

    verifyURL2

--- Middleware/sendEmail.js
+++ Middleware/sendEmail.js
@@ -3,7 +3,7 @@ const { Resend } = require("resend");
 const resend = new Resend(process.env.RESEND_API_KEY);
 
 const sendVerificationEmail = async (email, token) => {
- const verifyUrl = `${process.env.FRONTEND_VERIFY_URL}?token=${encodeURIComponent(token)}`;
+ const verifyUrl = `${process.env.VERIFY_URL}?token=${encodeURIComponent(token)}`;
 
 
   try {
